b84e4c3f65cd171969aa60ff8ccf9002820ab858
Abin Joseph <abinj30@gmail.com>
2020-04-26 16:29:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# tamilrockers-search
+
+CLI application to search tamilrockers.com.
+
+> Warning: This application doesn't bypass any regional restrictions to
+> tamilrockers.com. The application only works if you're in a country
+> where the website is accessible.
+
+## Usage Instructions
+
+    npm install tamilrockers-search -g
+    tamilrockers-search -l tamil -s "petta-2019" -o out.txt
+    tamilrockers-search -l tamil -s "vinnaithaandi varuvaayaa" -o out.txt
+    tamilrockers-search -l kannada -s "pailwan-2019" -o out.txt
+    tamilrockers-search -l malayalam -s "neram (2013)" -o out.txt
+    tamilrockers-search -l hindi -s "dangal-2016" -o out.txt
+
+Supported options:
+ -  Language: -l, --language
+ - Movie Name: -s, --searchString
+ - Output filename: -o, --output
+ 
+Supported Languages:
+ - Tamil
+ - Telugu
+ - Hindi
+ - Malayalam
+ - Kannada
---
+++ index.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const Fuse = require("fuse.js");
+const fs = require("fs");
+const url = require("./url.json");
+const { program } = require('commander');
+
+let matchCount = 0;
+let pageCount = 0;
+async function incrCounts(incr) {
+   pageCount += 1;
+   matchCount += incr;
+   process.stdout.write(`\r${pageCount} pages searched. Matches found: ${matchCount}`);
+}
+
+function getTitles($) {
+   const titlesRows = Array.from($(".topic_title"));
+   
+   return titlesRows.map(ele => {
+      const link = ele.attribs["href"];
+      const titleSpan = $(ele).find("span");
+      if(titleSpan.length > 0){
+         const title = titleSpan[0].firstChild.nodeValue;
+         return {link, title};
+      } else {
+         return {link: null, title: null};
+      }
+   });
+}
+
+
+function getMatches($, searchString) {
+   const titles = getTitles($);
+   let matches = [];
+
+   if (titles.length > 0) {
+      const fuse = new Fuse(titles, {keys: ["title"], threshold: 0.3});
+      matches = fuse.search(searchString).map(ele => ele.item);
+   }
+   return matches;
+}
+
+async function getPage(url) {
+
+   const nextNewSelector = ".topic_controls .next a";
+   const nextOldSelector = ".next";
+
+   try {
+      var htmlBody = await request(url);
+   } catch (error) {
+      throw new RequestError(error);
+   }
+   const $ = cheerio.load(htmlBody);
+
+   var nextPage = null;
+   if ($(nextNewSelector).length > 0) {
+      nextPage = $(nextNewSelector)[0].attribs["href"];
+   } 
+   else if($(nextOldSelector).length > 0) {
+      nextPage = $(nextOldSelector)[0].attribs["href"];
+   }
+
+   return {$, nextPage};
+}
+
+async function searchForum(url, searchString) {
+
+   const matchedTitles = new Array();
+   var nextPage = url;
+
+   while(nextPage != null) {
+      try {
+         var {$, nextPage} = await getPage(nextPage);
+      } catch (error) {
+         continue;
+      }
+      const matches = getMatches($, searchString);
+      incrCounts(matches.length);
+      matchedTitles.push(...matches);
+   }
+
+   return matchedTitles;
+}
+
+async function main(args){
+   
+   const {language, searchString, output} = args;
+   //language validation
+   let forumUrls;
+   if (language.toLowerCase() == "all") {
+      forumUrls = Object.keys(url).map(lang => url[lang]).reduce((cur, next) => cur.concat(next));
+   } else {
+      forumUrls = url[language.toLowerCase()];
+      if(forumUrls === undefined) {
+         console.log("Input Language not supported. Exiting...");
+         process.exit(1);
+      }
+   }
+   
+   //checking connection
+   try {
+      await getPage(forumUrls[0]);
+   } catch (error) {
+      console.log("Connection Failed. TamilRockers might be blocked in your region!");
+      process.exit(1);
+   }
+
+   const functions = forumUrls.map(url => searchForum(url, searchString));
+   Promise.all(functions).then(results => {
+      const matchedTitles = results.reduce((cur, next) => cur.concat(next));
+      if (matchedTitles.length == 0){
+         console.log("\nNo matches found."); return;
+      }
+      const fileOutputs = matchedTitles.map(ele => ele.link).join("\n");
+      fs.writeFileSync(output, fileOutputs);
+      console.log(`\nResults stored to ${output}`);
+   });
+}
+
+program
+   .requiredOption('-l, --language <type>', 'Movie Language')
+   .requiredOption('-s, --searchString <type>', 'Search String/Movie Name')
+   .requiredOption('-o, --output <type>', "Output File Name")
+program.parse(process.argv);
+
+main(program);
